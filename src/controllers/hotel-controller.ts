86b7a40919d6d29bf2d468a8c530ac18f024d2ec
Guilherme <guilherme98gssilva@gmail.com>
2023-02-06 16:40:32 -0300

    first commit

---
+++ src/controllers/hotel-controller.ts
@@ -0,0 +1,35 @@
+import { Response } from "express";
+import { AuthenticatedRequest } from "@/middlewares";
+import hotelService from "@/services/hotels-service";
+import httpStatus from "http-status";
+
+export async function getHotels(req: AuthenticatedRequest, res: Response) {
+
+   const { userId } = req;
+
+  try {
+   const hotels = await hotelService.getHotels(Number(userId));
+   return res.status(httpStatus.OK).send(hotels);
+ } catch (error) {
+   if (error.name === "NotFoundError") {
+     return res.sendStatus(httpStatus.NOT_FOUND);
+   }
+   return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+ }
+}
+
+export async function getHotelsWithRooms(req: AuthenticatedRequest, res: Response) {
+
+   const { hotelId } = req.params;
+   const { userId } = req;
+
+  try {
+   const hotels = await hotelService.getHotelsWithRooms(Number(userId), Number(hotelId));
+   return res.status(httpStatus.OK).send(hotels);
+ } catch (error) {
+   if (error.name === "NotFoundError") {
+     return res.sendStatus(httpStatus.NOT_FOUND);
+   }
+   return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+ }
+}
